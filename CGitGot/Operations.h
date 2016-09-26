a220fa9fa054a3387e1ba8587f50ec2b8d787ddb
Nate Hansen <nate1hansen@gmail.com>
2016-09-26 13:25:08 -0600

    Renaming to a more appropriate name for public repo

---
+++ CGitGot/Operations.h
@@ -0,0 +1,17 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+namespace got {
+
+	auto STATUS = "status";
+	auto PULL = "pull";
+
+	enum Operations
+	{
+		status = 0,
+		pull = 1
+	};
+}
+
+
+#endif
